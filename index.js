71d1bc26f0548287c02c946e1b89dcf2019ea42d
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-06 19:20:02 -0700

    put request updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = express();
 let users = [
   {
     id: 1,
-    name: "Eddie Madrigal",
+    name: "Eddie",
     bio: "Bio for Eddie"
   }
 ];
@@ -80,13 +80,50 @@ server.delete("/api/users/:id", (req, res) => {
     return user.id == userId;
   })[0];
 
-  const index = users.indexOf(user);
+  if (user) {
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    res
+      .json({ message: `User ${userId} has been deleted.` });
+  } else if (!user) {
+    res
+      .status(404)
+      .json({ message: `The user with the specified ID does not exist.` })
+  } else {
+    res
+      .status(500)
+      .json({ errorMessage: `The user could not be removed.` })
+  }
 
-  users.splice(index, 1);
+})
 
-  res
-    .json({ message: `User ${userId} has been deleted.` })
+// PUT requests
 
+server.put("/api/users/:id", (req, res) => {
+    
+  const updateUser = req.body;
+  const userId = req.params.id;
+  const oldUser = users.find(i => i.id === Number(userId));
+
+  if (users.find(i => i.id == (userId))) {
+    if (req.body.name.length > 0 || req.body.bio.length > 0) {
+      if (oldUser === updateUser) {
+        res.status(500).json({ errorMessage: "The user information could not be found." })
+      } else {
+        updateUser.id = users.length
+        users = [
+          ...users.slice(0, oldUser.id),
+          updateUser,
+          users.slice(oldUser.id + 1)
+        ]
+        res.status(200).json(users)
+      }
+    } else {
+      res.status(400).json({ errorMessage: "Please provide a name and bio for the user." })
+    }
+  } else {
+    res.status(404).json({ message: "The user with the specified id does not exist" })
+  }
 })
 
 const port = 5000;
